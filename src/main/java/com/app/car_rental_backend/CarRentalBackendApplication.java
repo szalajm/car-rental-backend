92920df132b6e2172df006e3322fae50ac89d3c2
szalajm <szalajm.neo@gmail.com>
2019-11-27 14:54:56 +0100

    test4

--- src/main/java/com/app/car_rental_backend/CarRentalBackendApplication.java
+++ src/main/java/com/app/car_rental_backend/CarRentalBackendApplication.java
@@ -1,7 +1,10 @@
 package com.app.car_rental_backend;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CarRentalBackendApplication {
--- src/test/java/com/app/car_rental_backend/CarRentalBackendApplicationTests.java
+++ src/test/java/com/app/car_rental_backend/CarRentalBackendApplicationTests.java
@@ -2,7 +2,10 @@ package com.app.car_rental_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ContextConfiguration
 @SpringBootTest
 class CarRentalBackendApplicationTests {
 
