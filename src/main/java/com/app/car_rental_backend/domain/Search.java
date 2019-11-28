0aa7cdd8310b58d1ee388fa4a9b442808baef0c6
szalajm <szalajm.neo@gmail.com>
2019-11-28 19:22:26 +0100

    klasy_1

--- src/main/java/com/app/car_rental_backend/domain/Search.java
+++ src/main/java/com/app/car_rental_backend/domain/Search.java
@@ -1,4 +1,29 @@
 package com.app.car_rental_backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Search {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToMany
+    @JoinColumn(name = "carId")
+    private List<Car> selectedCars = new ArrayList<>();
+
+    public void selectCriteria(){
+        System.out.println("builder to be implemented");
+    }
 }
