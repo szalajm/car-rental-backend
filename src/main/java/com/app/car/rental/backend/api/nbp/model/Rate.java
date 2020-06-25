c250c4dbcb9bb068e07597ff0dfbb5981e8e0df8
szalajm <szalajm.neo@gmail.com>
2020-06-25 16:30:48 +0200

    Nbp Api Model Rate

---
+++ src/main/java/com/app/car/rental/backend/api/nbp/model/Rate.java
@@ -0,0 +1,77 @@
+package com.app.car.rental.backend.api.nbp.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "currency",
+        "code",
+        "mid"
+})
+public class Rate {
+
+    @JsonProperty("currency")
+    private String currency;
+    @JsonProperty("code")
+    private String code;
+    @JsonProperty("mid")
+    private Double mid;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("currency")
+    public String getCurrency() {
+        return currency;
+    }
+
+    @JsonProperty("currency")
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @JsonProperty("code")
+    public String getCode() {
+        return code;
+    }
+
+    @JsonProperty("code")
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @JsonProperty("mid")
+    public Double getMid() { return mid; }
+
+    @JsonProperty("mid")
+    public void setMid(Double mid) {
+        this.mid = mid;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Rate{" +
+                "currency='" + currency + '\'' +
+                ", code='" + code + '\'' +
+                ", mid=" + mid +
+                ", additionalProperties=" + additionalProperties +
+                '}';
+    }
+}
