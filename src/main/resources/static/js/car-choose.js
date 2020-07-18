a4856db22d4b33f915b8d98339b54119866e785b
Maciej Szałaj <szalajm.neo@gmail.com>
2020-07-18 10:40:32 +0200

    Add JQuery to toggle background color for selected Car

--- src/main/resources/static/js/car-choose.js
+++ src/main/resources/static/js/car-choose.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-    $(".chosenCar").click(function() {
+    $(".chooseCar").click(function(e) {
         var categoryName = $(this).closest("td").find("input[name='categoryName']").val();
         var categoryMake = $(this).closest("td").find("input[name='categoryMake']").val();
         var vehicleClassCode = $(this).closest("td").find("input[name='vehicleClassCode']").val();
 
-        console.log(categoryName);
-        console.log(categoryMake);
-        console.log(vehicleClassCode);
+        $("div.chooseCar").removeClass("chosenCar");
+        $(this).addClass("chosenCar");
 
         $.get("/v1/rental/cars/choose", {
                 categoryName : categoryName,
